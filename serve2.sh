58d93180c7a244a2d9c89979b5a4c09385ac49ad
dingruiyi <dingruiyi@163.comi>
2025-12-07 12:49:54 +0800

    Remove unused files including `olymmath.py`, `prm.yaml`, `req.txt`, `rewardManager.py`, `serve2.sh`, `verl.yaml`, and `verl.yml`. Update `.gitignore` to include additional file types.

--- serve2.sh
+++
@@ -1,4 +0,0 @@
-source /mnt/shared-storage-user/mineru4s/dingruiyi/anaconda/bin/activate
-conda activate prm
-cd /mnt/shared-storage-user/mineru4s/dingruiyi/srpo
-gunicorn -k gevent -w 8 --worker-connections 1000 -b 0.0.0.0:4997 wsgi:app
